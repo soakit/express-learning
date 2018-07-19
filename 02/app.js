3f75069a08e1cf433732287a53ffd92dfef3b71b
soakit <liuchang2024@gmail.com>
2018-07-19 10:49:53 +0800

    let browser supports non-GET and non-POST

--- 02/app.js
+++ 02/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +19,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// 方式1
+app.use(function(req, res, next) {
+  req.oldMethod = req.method
+  // 客户端传递__method参数
+  req.method = req.query.__method
+  next()
+})
+// 方式2
+// method-override中间件也可实现
+// X-HTTP-Method-Override 在request的header中加入
+app.use(methodOverride('X-HTTP-Method-Override'))
+
+
+// 支持search方法
+app.search('/', function(req, res) {
+  res.send('这是search方法')
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
